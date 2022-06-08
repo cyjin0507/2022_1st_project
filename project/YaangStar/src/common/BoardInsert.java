6ec68b7d62f491aefd92af28d1f45c36fc8f8c88
gmlxo <msaaa794274@gmail.com>
2022-06-08 10:39:15 +0900

    .

---
+++ project/YaangStar/src/common/BoardInsert.java
@@ -0,0 +1,78 @@
+package common;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Date;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.oreilly.servlet.MultipartRequest;
+import com.oreilly.servlet.multipart.DefaultFileRenamePolicy;
+
+@WebServlet("/boardInsert")
+public class BoardInsert extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	public BoardInsert() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		request.setCharacterEncoding("UTF-8");
+		response.setContentType("text/html; charset=UTF-8");
+
+		PrintWriter out = response.getWriter();
+
+		String image, tage, userContent, reserved1, reserved2;
+
+		int idx;
+		int uidx = 0;
+		int c = 0;
+
+		MemberDAO dao = new MemberDAO();
+		Date create_date;
+		create_date = dao.myDate();
+
+		idx = dao.getLastIdxBoard() + 1;
+		uidx = dao.getLastUidxBoard() + 1;
+
+		/*
+		 * 지정된 경로에 이미지 넣는거 지정된 경로 :
+		 * C:/Users/User/Desktop/TeamProject_1stSemester/2022_1st_project/project/
+		 * YaangStar/WebContent/resources/upload/imageBoard
+		 */
+		
+		MultipartRequest mr = new MultipartRequest(request,
+				"C:/Users/User/Desktop/TeamProject_1stSemester/2022_1st_project/project/YaangStar/WebContent/resources/upload/imageBoard",
+				1024 * 1024 * 1024, "utf-8", new DefaultFileRenamePolicy());
+
+		String fileName = mr.getFilesystemName("image");
+		System.out.println("사진 이름 : " + fileName);
+
+		image = "/resources/upload/imageBoard/" + fileName;
+		System.out.println("이미지 경로 : " + image);
+
+		tage = request.getParameter("tage");
+		userContent = request.getParameter("userContent");
+
+		reserved1 = "";
+		reserved2 = "";
+
+		System.out.println("idx : " + idx);
+		System.out.println("uidx : " + uidx);
+
+		c = dao.insertBoard(idx, uidx, tage, userContent, image, create_date, reserved1, reserved2);
+
+		if (c < 0)
+			out.print("버그");
+		out.print("<script> History.back() </script>");
+	}
+
+}
