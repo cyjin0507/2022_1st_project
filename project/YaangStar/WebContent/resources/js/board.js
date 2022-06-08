a04cc02fc78c797aebfe6c864b0022338115fbfa
cyjin0507 <cyjin0507@gmail.com>
2022-06-08 15:06:01 +0900

    유저 정보 변경

--- project/YaangStar/WebContent/resources/js/board.js
+++ project/YaangStar/WebContent/resources/js/board.js
@@ -1,3 +1,5 @@
+
+// index.jsp 게시판 이미지 등록시 사용
 $("#imgFile").on('change',()=>{
       let selectFile = document.querySelector("#imgFile").files[0];
 
@@ -5,5 +7,15 @@ $("#imgFile").on('change',()=>{
 
       $('#board-img').css('background-image', 'url('+ file +')')
       
+
+})
+
+//userController.jsp 게시판 이미지 등록시 사용
+$("#img_control").on('change',()=>{
+      let selectFile = document.querySelector("#img_control").files[0];
+
+      const file = URL.createObjectURL(selectFile);
+
+      document.querySelector("#img-control-img").src = file;
 
 })
