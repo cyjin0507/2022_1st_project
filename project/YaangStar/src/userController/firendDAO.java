63b6a9e8a6bd33cc2bc6d97267df3c356ea39402
gmlxo <msaaa794274@gmail.com>
2022-06-15 10:23:56 +0900

    commit

--- project/YaangStar/src/userController/firendDAO.java
+++ project/YaangStar/src/userController/firendDAO.java
@@ -4,13 +4,40 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 import dao.JdbcUtil;
 import dao.MemberDAO;
 
 public class firendDAO {
 	
-	public boolean insertFollows(int userId1) {
+	public int getLastIdxFirend() {
+		int a = 0;
+
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+
+			String url = "jdbc:oracle:thin:@pukkuk.pp.ua:49161:xe";
+			Connection conn = JdbcUtil.getConnection();
+
+			Statement stmt = conn.createStatement();
+
+			// 최근에 넣은 idx 값 가져 오기
+			ResultSet rs = stmt
+					.executeQuery("SELECT * FROM (SELECT idx FROM friendTable ORDER BY idx DESC) WHERE ROWNUM=1");
+
+			while (rs.next()) {
+				System.out.println("idx (commentTable)  값 가져오기 성공");
+				a = rs.getInt("idx");
+			}
+			conn.close();
+		} catch (Exception e) {
+			System.out.println("idx (commentTable) 값 가져오기 실패");
+		}
+		return a;
+	}
+	
+	public boolean insertFollows(String idx, int userId1, String myIdx) {
 		int n = 0;
 		
 		// db 연동
@@ -23,10 +50,10 @@ public class firendDAO {
 		MemberDAO dao = new MemberDAO();
 		try {
 			pstmt = conn.prepareStatement(sql);
-			pstmt.setInt(1, 1); // idx +1 
+			pstmt.setString(1, idx); // idx +1 
 			pstmt.setInt(2,userId1); //
-			pstmt.setInt(3, 1);// 내 아이디 세션
-			pstmt.setString(4, "false");
+			pstmt.setString(3, myIdx);// 내 아이디 세션
+			pstmt.setString(4, "true");
 			n =pstmt.executeUpdate();
 			
 			return true;
--- project/YaangStar/src/userController/friendAdd.java
+++ project/YaangStar/src/userController/friendAdd.java
@@ -8,39 +8,36 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 @WebServlet("/friendAdd")
 public class friendAdd extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    public friendAdd() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	public friendAdd() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		request.setCharacterEncoding("UTF-8");
 		response.setContentType("text/html; charset=UTF-8");
 		PrintWriter out = response.getWriter();
+		HttpSession session = request.getSession();
 		int friendId = Integer.parseInt(request.getParameter("id"));
 		firendDAO friendListUpdate = new firendDAO();
-		boolean updateCheck = friendListUpdate.insertFollows(friendId);
+		String idx = friendListUpdate.getLastIdxFirend() + 1 + "";
+		String myIdx = (String) session.getAttribute("logOK");
 		
-		if(updateCheck) {
-			out.print("성공");
+		boolean updateCheck = friendListUpdate.insertFollows(idx, friendId, myIdx);
+
+		if (updateCheck) {
+			response.sendRedirect("main.jsp");
+			out.println("<script> alert(\"성공\");</script>");
 		} else {
-			out.print("실패");
+			out.println("<script> alert(\"실패\"); history.go(-1); </script>");
 		}
-		
-		
-	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
 	}
-
 }
